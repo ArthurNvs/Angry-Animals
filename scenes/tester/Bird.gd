a315771c628468ed93bfb3a935b2071d02d54718
Arthur <arthurnvsm@gmail.com>
2024-06-16 00:51:33 -0400

    first commit

---
+++ scenes/tester/Bird.gd
@@ -0,0 +1,15 @@
+extends RigidBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _pro_cess(_delta):
+	pass
+
+
+func _on_input_event(_viewport, event, _shape_idx):
+	if event.button_mask == 1:
+		position = get_global_mouse_position()
